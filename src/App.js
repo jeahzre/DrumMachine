70b922552e88e3355d13e3b5e37aabf36eaeb7b2
jeatz <83843786+jeatz@users.noreply.github.com>
2021-10-17 21:13:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import './App.css'
 
-function App() {
+const DrumPad = ({drumpad, audioName, audio, handleClick}) => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {
+      drumpad.map((pad, index) => {
+        return (
+      <button className="drum-pad button btn-primary" onClick={(e) => handleClick(e)}
+      id={audioName[index]} key={index}>{pad}<audio className="clip" id={pad} src={audio[index]} preload="auto"></audio></button>
+      );
+      })
+      }
+  </>
+    )
 }
 
-export default App;
+const DrumMachine = () => {
+  const [clicked, setClicked] = useState('')
+  const drumpad = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
+  const audioName = ['clap', 'crash', 'hi-hat1', 'hi-hat2', 'kick1', 'kick2', 'perc1', 'perc2', 'snare'];
+  const audio = [
+    'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3', 
+    'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3', 
+    'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+    'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3', 
+    'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3', 
+    'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+    'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3', 
+    'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'];
+  
+  const handleClick = (e, key) => {
+    if(e) {
+    const audioKey = e.target.children[0].id;
+    const audioNameIndex = drumpad.indexOf(audioKey);
+    const audioEl = document.getElementById(audioKey);
+    audioEl.currentTime = 0;
+    setClicked(audioName[audioNameIndex]);
+    return audioEl.play();
+    } else {
+      const clickedPad = document.getElementById(key);
+      const audioNameIndex = drumpad.indexOf(key);
+      setClicked(audioName[audioNameIndex]);
+      clickedPad.currentTime = 0;
+      return clickedPad.play();
+    }
+  };
+  
+  const handleKeyDown = (e) => {
+    for(let key of drumpad) {
+      if(e.key === `${key.toLowerCase()}`) {
+        handleClick(null, key);
+      }
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    
+    return (() => { 
+    return window.removeEventListener('keydown', handleKeyDown)})
+  });
+  
+  return (
+    <div className="card mx-auto d-flex justify-content-center align-items-center" id="drum-machine">
+    <div className="card-body">
+    <DrumPad drumpad={drumpad} audio={audio} handleClick={handleClick} audioName={audioName}/>
+    <div className="d-flex justify-content-center" id="display-container "><span className="badge badge-info d-flex justify-content-center align-items-center" id="display">{clicked}</span></div>
+    </div>
+    </div >
+    )
+};
+
+export default DrumMachine;
